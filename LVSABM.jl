c5e6bd590d4ebe94a47022fecb6553a1e1162d88
owensd2 <91539255+owensd2@users.noreply.github.com>
2022-05-18 14:57:26 +0100

    Update LVSABM.jl

--- LVSABM.jl
+++ LVSABM.jl
@@ -12,10 +12,6 @@
 # Copyright (c) 2022, Daniel Owens, Indecon International Economic Consultants
 # All rights reserved.
 
-using Pkg
-
-Pkg.add(["Agents", "Random", "StatsBase", "Distributions", "Dates", "GLMakie", "Makie", "Plots", "Colors"])
-
 using Agents, Random, StatsBase, Distributions, Dates
 
 # Buyer agent and associated values
